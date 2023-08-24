ccbfa215f61a96c6ff326bd8cf7f4dd9891bf1f6
HighRollerLV <ip19.r.dambitis@vtdt.edu.lv>
2023-08-24 15:00:56 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p class={"text-green-50"}>
+        <p class={"text-green-500"}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
