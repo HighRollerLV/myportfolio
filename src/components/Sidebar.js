6d9724890c4e6cd7fbde0d0969c94bdd08b1dddd
HighRollerLV <ip19.r.dambitis@vtdt.edu.lv>
2023-08-24 18:49:24 +0300

    SideBar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <aside
+            id="default-sidebar"
+            className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+            aria-label="Sidebar"
+        >
+        </aside>
+    );
+};
+
+export default Sidebar;
