4b567755218c7fe7227ea6a4a4714ef9cddb5c7e
HighRollerLV <ip19.r.dambitis@vtdt.edu.lv>
2023-08-28 01:54:23 +0300

    Links and Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import SidebarItem from './components/SidebarItem';
 import SectionTitle from './components/SectionTitle';
@@ -26,36 +27,49 @@ import Contact from './components/pages/Contact';
 
 const App = () => {
     return (
-        <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
-            <Sidebar />
-            <div className="overflow-y-auto overflow-x-hidden flex-grow">
-                <ul className="flex flex-col py-4 space-y-1">
-                    <SectionTitle sectionName="MyPortfolio" />
-                    {/* Main Section */}
-                    <SectionTitle sectionName="Main" />
-                    <SidebarItem icon={<UilEstate />} text="Home" />
-                    <SidebarItem icon={<UilUser />} text="About" />
-                    <SidebarItem icon={<UilBookAlt />} text="Education" />
+        <Router>
+            <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
+                <Sidebar/>
+                <div className="overflow-y-auto overflow-x-hidden flex-grow">
+                    <ul className="flex flex-col py-4 space-y-1">
+                        <SectionTitle sectionName="MyPortfolio"/>
+                        {/* Main Section */}
+                        <SectionTitle sectionName="Main"/>
+                        <SidebarItem icon={<UilEstate/>} text="Home" to="/"/>
+                        <SidebarItem icon={<UilUser/>} text="About" to="/about"/>
+                        <SidebarItem icon={<UilBookAlt/>} text="Education" to="/education"/>
 
-                    {/* Journey Section */}
-                    <SectionTitle sectionName="Journey" />
-                    <SidebarItem icon={<UilBracketsCurly />} text="Projects" />
-                    <SidebarItem icon={<UilFlask />} text="Experience" />
-                    <SidebarItem icon={<UilHourglass />} text="Timeline" />
+                        {/* Journey Section */}
+                        <SectionTitle sectionName="Journey"/>
+                        <SidebarItem icon={<UilBracketsCurly/>} text="Projects" to="/projects"/>
+                        <SidebarItem icon={<UilFlask/>} text="Experience" to="/experience"/>
+                        <SidebarItem icon={<UilHourglass/>} text="Timeline" to="/timeline"/>
 
-                    {/* Contact Section */}
-                    <SectionTitle sectionName="Contact" />
-                    <SidebarItem icon={<UilUserSquare />} text="Contact" />
+                        {/* Contact Section */}
+                        <SectionTitle sectionName="Contact"/>
+                        <SidebarItem icon={<UilUserSquare/>} text="Contact" to="/contact"/>
 
-                    {/* Media Section */}
-                    <SectionTitle sectionName="Media" />
-                    <SidebarItem icon={<UilLinkedin />} text="LinkedIn" link="https://www.linkedin.com/in/ralfs-dambitis-880369264/" />
-                    <SidebarItem icon={<UilInstagram />} text="Instagram" link="https://www.instagram.com/ralfs_d/" />
-                    <SidebarItem icon={<UilFacebook />} text="Facebook" link="https://www.facebook.com/ralfs.dambitis" />
-                    <SidebarItem icon={<UilTwitter />} text="Twitter" link="https://twitter.com/RalfsDambitis" />
-                </ul>
+                        {/* Media Section */}
+                        <SectionTitle sectionName="Media"/>
+                        <SidebarItem icon={<UilLinkedin/>} text="LinkedIn"
+                                     link="https://www.linkedin.com/in/ralfs-dambitis-880369264/"/>
+                        <SidebarItem icon={<UilInstagram/>} text="Instagram" link="https://www.instagram.com/ralfs_d/"/>
+                        <SidebarItem icon={<UilFacebook/>} text="Facebook"
+                                     link="https://www.facebook.com/ralfs.dambitis"/>
+                        <SidebarItem icon={<UilTwitter/>} text="Twitter" link="https://twitter.com/RalfsDambitis"/>
+                    </ul>
+                </div>
             </div>
-        </div>
+            <Routes>
+                <Route path="/" exact component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/education" component={Education}/>
+                <Route path="/experience" component={Experience}/>
+                <Route path="/projects" component={Projects}/>
+                <Route path="/timeline" component={Timeline}/>
+                <Route path="/contact" component={Contact}/>
+            </Routes>
+        </Router>
     );
 };
 
--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SidebarItem = ({icon, text, isNew, link}) => {
+const SidebarItem = ({ icon, text, isNew, to, link }) => {
+    if (link) {
+        // External link, open in a new tab
+        return (
+            <li>
+                <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="relative flex items-center h-11 pl-4 hover:bg-gray-50 text-gray-600 hover:text-gray-800 transition-all duration-300 ease-in-out w-64"
+                >
+                    <span className="inline-flex justify-center items-center">{icon}</span>
+                    <span className="ml-4 text-xl tracking-wide truncate">{text}</span>
+                    {isNew && (
+                        <span
+                            className="absolute top-1 right-1 px-2 py-0.5 text-xs font-medium tracking-wide text-indigo-500 bg-indigo-50 rounded-full"
+                        >
+              New
+            </span>
+                    )}
+                </a>
+            </li>
+        );
+    }
+
+    // Internal link
     return (
         <li>
-            <a
-                href={link}
-                target="_blank"
-                rel="noopener noreferrer"
+            <Link
+                to={to}
                 className="relative flex items-center h-11 pl-4 hover:bg-gray-50 text-gray-600 hover:text-gray-800 transition-all duration-300 ease-in-out w-64"
             >
                 <span className="inline-flex justify-center items-center">{icon}</span>
                 <span className="ml-4 text-xl tracking-wide truncate">{text}</span>
                 {isNew && (
                     <span
-                        className="absolute top-1 right-1 px-2 py-0.5 text-xs font-medium tracking-wide text-indigo-500 bg-indigo-50 rounded-full">
-                        New
-                    </span>
+                        className="absolute top-1 right-1 px-2 py-0.5 text-xs font-medium tracking-wide text-indigo-500 bg-indigo-50 rounded-full"
+                    >
+            New
+          </span>
                 )}
-            </a>
+            </Link>
         </li>
     );
 };
